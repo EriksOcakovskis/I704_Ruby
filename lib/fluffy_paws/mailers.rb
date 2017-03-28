abccb1d32e9ea409981e69d9df0cf63c46110915
Eriks Ocakovskis <e.ocakovskis@gmail.com>
2017-03-28 23:10:11 +0300

    Change server url to environmental variable

--- lib/fluffy_paws/mailers.rb
+++ lib/fluffy_paws/mailers.rb
@@ -7,11 +7,12 @@ module FluffyPaws
         from = Email.new(email: 'app65106927@heroku.com')
         subject = "Hi #{params[:user]}, your token for FluffyPaws"
         to = Email.new(email: params[:recipient])
-        content = Content.new(type: 'text/html',
-                              value: "<html><p>Click the link below to login</p>
-                              <a href=\"http://localhost:9292/login/
-                              #{params[:token]}\">Click Me!</a>
-                              <p>FluffyPaws team</p></html>")
+        content = Content.new(
+          type: 'text/html',
+          value: "<html><p>Click the link below to login</p>
+          <a href=\"#{ENV.fetch('SERVER_URL')}/login/#{params[:token]}\">Click Me!</a>
+          <p>FluffyPaws team</p></html>"
+        )
         mail = Mail.new(from, subject, to, content)
 
         sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
