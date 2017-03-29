01151514b95020a2c4c39b4bdb2a15a05d1494d1
Eriks Ocakovskis <e.ocakovskis@gmail.com>
2017-03-29 15:12:41 +0300

    Adjust tests for user authorisation changes

--- test/fluffy_paws_test.rb
+++ test/fluffy_paws_test.rb
@@ -12,26 +12,19 @@ class FluffyPawsTest < Minitest::Test
   end
 
   def test_index_response_is_ok
-    get '/'
+    get '/login'
     assert last_response.ok?
   end
 
   def test_index_body_contains_custom_title
-    get '/'
-    assert_includes last_response.body, 'Holy Smokes!'
+    get '/login'
+    assert_includes last_response.body, 'Fluffy Paws'
   end
 
   def test_logout_redirects_to_index
     get '/logout'
     follow_redirect!
     assert last_response.ok?
-    assert_equal 'http://example.org/', last_request.url
-  end
-
-  def test_login_redirects_to_index
-    post '/login'
-    follow_redirect!
-    assert last_response.ok?
-    assert_equal 'http://example.org/', last_request.url
+    assert_equal 'http://example.org/login', last_request.url
   end
 end
